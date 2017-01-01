b2ac004efe8d7558696fd12d6bc1a19e43ca8b6f
Jakub Giminski <jakubgiminski@gmail.com>
2017-01-01 19:08:13 +0000

    Adds new static constructors to GameStatus value object

--- TurnsGame/GameStatus.php
+++ TurnsGame/GameStatus.php
@@ -6,10 +6,20 @@ use ValueObject\StringValue\StringValue;
 
 class GameStatus extends StringValue
 {
-    protected $allowedValues = ['unstarted', 'ongoing', 'finished'];
+    protected static $allowedValues = ['unstarted', 'ongoing', 'finished'];
 
     public static function ongoing()
     {
         return new self('ongoing');
+    }
+
+    public static function unstarted()
+    {
+        return new self('unstarted');
+    }
+
+    public static function finished()
+    {
+        return new self('finished');
     }
 }
